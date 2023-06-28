20b7708ee373a3c0404e344d8992951512cf4f48
Bolanle <bolanlesamuell@gmail.com>
2023-06-28 03:47:56 -0700

    main.h

--- main.h
+++ main.h
@@ -7,14 +7,18 @@
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
 
-/* FLAGS */
+/**  
+ * FLAGS
+ */
 #define F_MINUS 1
 #define F_PLUS 2
 #define F_ZERO 4
 #define F_HASH 8
 #define F_SPACE 16
 
-/* SIZES */
+/** 
+ * SIZE
+ */
 #define S_LONG 2
 #define S_SHORT 1
 
@@ -43,7 +47,9 @@ int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
-/****************** FUNCTIONS ******************/
+/**
+ * FUNCTIONS
+ */
 
 /* Funtions to print chars and strings */
 int print_char(va_list types, char buffer[],
@@ -106,7 +112,9 @@ int write_unsgnd(int is_negative, int ind,
 char buffer[],
 	int flags, int width, int precision, int size);
 
-/****************** UTILS ******************/
+/**
+ * UTILS
+ */
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
@@ -114,4 +122,4 @@ int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
-#endif /* MAIN_H */
+#endif
